f73d4142f68bc3f984b32a34ee26af5ea0c3be32
Muga Nishizawa <muga.nishizawa@gmail.com>
2013-01-20 20:59:28 +0900

    fixed it

--- src/test/java/org/apache/catalina/util/TestStrftime.java
+++ src/test/java/org/apache/catalina/util/TestStrftime.java
@@ -4,6 +4,7 @@ import static org.junit.Assert.assertEquals;
 
 import java.text.SimpleDateFormat;
 
+import org.junit.Ignore;
 import org.junit.Test;
 
 public class TestStrftime {
@@ -11,11 +12,11 @@ public class TestStrftime {
     @Test
     public void parseText() throws Exception {
         long t = 1358675873;
-        String pat = "%Y-%m-%d %X";
+        String pat = "%Y-%m-%d %X %z";
         Strftime strftime = new Strftime(pat);
         String converted = strftime.convertDateFormat(pat);
         System.out.println(converted);
         SimpleDateFormat f = new SimpleDateFormat(converted);
-        assertEquals(t, f.parse("2013-01-20 18:57:53").getTime() / 1000);
+        assertEquals(t, f.parse("2013-01-20 18:57:53 JST").getTime() / 1000);
     }
 }
