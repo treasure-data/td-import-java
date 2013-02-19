7b2202dac1be370994f5e8942bafa6c067a7aee5
Muga Nishizawa <muga.nishizawa@gmail.com>
2013-02-19 12:59:14 +0900

    implemented upload-parts

--- src/main/java/com/treasure_data/commands/bulk_import/UploadPartsCommand.java
+++ src/main/java/com/treasure_data/commands/bulk_import/UploadPartsCommand.java
@@ -17,10 +17,26 @@
 //
 package com.treasure_data.commands.bulk_import;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.BufferedInputStream;
+import java.io.InputStream;
+import java.util.logging.Logger;
+
+import com.treasure_data.client.ClientException;
+import com.treasure_data.client.RetryClient;
+import com.treasure_data.client.RetryClient.Retryable;
+import com.treasure_data.client.TreasureDataClient;
+import com.treasure_data.client.bulkimport.BulkImportClient;
 import com.treasure_data.commands.Command;
 import com.treasure_data.commands.CommandException;
+import com.treasure_data.model.bulkimport.Session;
+import com.treasure_data.model.bulkimport.UploadPartRequest;
 
 public class UploadPartsCommand extends Command<UploadPartsRequest, UploadPartsResult> {
+    private static final Logger LOG = Logger
+            .getLogger(UploadPartsCommand.class.getName());
 
     public UploadPartsCommand() {
     }
@@ -28,7 +44,42 @@ public class UploadPartsCommand extends Command<UploadPartsRequest, UploadPartsR
     @Override
     public void execute(UploadPartsRequest request, UploadPartsResult result)
             throws CommandException {
-        // TODO
+        File[] files = request.getFiles();
+        for(File f : files) {
+            execute(request, result, f);
+        }
     }
 
+    private void execute(final UploadPartsRequest request,
+            final UploadPartsResult result, final File file)
+            throws CommandException {
+        LOG.info(String.format("started uploading file: %s", file.getName()));
+
+        final TreasureDataClient client = new TreasureDataClient(request.getProperties());
+        final BulkImportClient biClient = new BulkImportClient(client);
+        try {
+            new RetryClient().retry(new Retryable() {
+                @Override
+                public void doTry() throws ClientException {
+                    try {
+                        Session sess = new Session(request.getSessionName(), null, null);
+                        String partID = file.getName();
+                        InputStream in = new BufferedInputStream(
+                                new FileInputStream(file));
+                        long size = file.length();
+
+                        UploadPartRequest req = new UploadPartRequest(sess,
+                                partID, in, (int) size);
+                        // TODO #MN change client's API: int to long
+                        biClient.uploadPart(req);
+                    } catch (IOException e) {
+                        throw new ClientException(e);
+                    }
+                }
+            }, request.getRetryCount(), request.getWaitSec());
+        } catch (IOException e) {
+            LOG.severe(e.getMessage());
+            throw new CommandException(e);
+        }
+    }
 }
--- src/main/java/com/treasure_data/commands/bulk_import/UploadPartsRequest.java
+++ src/main/java/com/treasure_data/commands/bulk_import/UploadPartsRequest.java
@@ -17,20 +17,37 @@
 //
 package com.treasure_data.commands.bulk_import;
 
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Properties;
+import java.util.logging.Logger;
 
 import com.treasure_data.commands.CommandException;
 import com.treasure_data.commands.CommandRequest;
+import com.treasure_data.commands.Config;
 
 public class UploadPartsRequest extends CommandRequest {
+    private static final Logger LOG = Logger
+            .getLogger(UploadPartsRequest.class.getName());
+
     private static final String COMMAND_NAME = "upload_parts";
 
+    protected String sessionName;
+    protected File[] files;
+    protected boolean autoPerform;
+    protected boolean autoCommit;
+    protected int retryCount;
+    protected long waitSec;
+
+    //bulk_import:upload <name> <raw file paths…> [--auto-perform=true] [--auto-commit=false] [PREPARE_PARTS_OPTIONS]
     public UploadPartsRequest() throws CommandException {
         super(null);
     }
 
-    public UploadPartsRequest(String[] fileNames, Properties props) throws CommandException {
+    public UploadPartsRequest(String sessionName, String[] fileNames, Properties props) throws CommandException {
         super(props);
+        setSessionName(sessionName);
         setFiles(fileNames);
         setOptions(props);
     }
@@ -40,11 +57,74 @@ public class UploadPartsRequest extends CommandRequest {
         return COMMAND_NAME;
     }
 
+    protected void setSessionName(String sessionName) {
+        this.sessionName = sessionName;
+    }
+
+    protected String getSessionName() {
+        return sessionName;
+    }
+
     protected void setFiles(String[] fileNames) throws CommandException {
-        // TODO override?
+        // validation for file names
+        List<File> fileList = new ArrayList<File>(fileNames.length);
+        for (int i = 0; i < fileNames.length; i++) {
+            String fname = fileNames[i];
+            File f = new File(fname);
+            if (!f.isFile()) {
+                LOG.severe("No such file: " + fname);
+            } else {
+                fileList.add(f);
+            }
+        }
+        files = fileList.toArray(new File[0]);
+    }
+
+    protected File[] getFiles() {
+        return files;
     }
 
     protected void setOptions(Properties props) throws CommandException {
-        // TODO override?
+        // auto-perform
+        String aperform = props.getProperty(Config.BI_UPLOAD_PARTS_AUTO_PERFORM,
+                Config.BI_UPLOAD_PARTS_AUTO_PERFORM_DEFAULTVALUE);
+        autoPerform = aperform != null && aperform.equals("true");
+
+        // auto-commit
+        String acommit = props.getProperty(Config.BI_UPLOAD_PARTS_AUTO_COMMIT,
+                Config.BI_UPLOAD_PARTS_AUTO_COMMIT_DEFAULTVALUE);
+        autoCommit = aperform != null && acommit.equals("true");
+
+        // retryCount
+        String rcount = props.getProperty(Config.BI_UPLOAD_PARTS_RETRYCOUNT,
+                Config.BI_UPLOAD_PARTS_RETRYCOUNT_DEFAULTVALUE);
+        try {
+            retryCount = Integer.parseInt(rcount);
+        } catch (NumberFormatException e) {
+            String msg = String.format(
+                    "retry count is required as int type e.g. -D%s=5",
+                    Config.BI_UPLOAD_PARTS_RETRYCOUNT);
+            throw new CommandException(msg, e);
+        }
+
+        // waitSec
+        String wsec = props.getProperty(Config.BI_UPLOAD_PARTS_WAITSEC,
+                Config.BI_UPLOAD_PARTS_WAITSEC_DEFAULTVALUE);
+        try {
+            waitSec = Long.parseLong(wsec);
+        } catch (NumberFormatException e) {
+            String msg = String.format(
+                    "wait sec is required as long type e.g. -D%s=5",
+                    Config.BI_UPLOAD_PARTS_WAITSEC);
+            throw new CommandException(msg, e);
+        }
+    }
+
+    public int getRetryCount() {
+        return retryCount;
+    }
+
+    public long getWaitSec() {
+        return waitSec;
     }
 }
