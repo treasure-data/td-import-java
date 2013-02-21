597705f6316629f56c16f4c794b1174915ef0e9c
Muga Nishizawa <muga.nishizawa@gmail.com>
2013-02-21 17:21:10 +0900

    added new factory class for creating UploadParts{Request,Result,Command} classes: UploadPartsFactory class

---
+++ src/main/java/com/treasure_data/commands/bulk_import/UploadPartsFactory.java
@@ -0,0 +1,57 @@
+package com.treasure_data.commands.bulk_import;
+
+import java.util.Properties;
+import java.util.logging.Logger;
+
+import com.treasure_data.commands.CommandException;
+
+public class UploadPartsFactory {
+
+    private static final Logger LOG = Logger
+            .getLogger(UploadPartsFactory.class.getName());
+
+    public static UploadPartsRequest newRequestInstance(String sessionName,
+            String[] fileNames, Properties props) throws CommandException {
+
+        boolean autoPrepare = false;
+        for (Object key : props.keySet()) {
+            String val = props.getProperty((String) key);
+            if (val != null && val.startsWith("td.bulk_import.prepare_parts")) {
+                autoPrepare = true;
+                break;
+            }
+        }
+
+        if (autoPrepare) {
+            return new PrepareUploadPartsRequest(sessionName, fileNames, props);
+        } else {
+            return new UploadPartsRequest(sessionName, fileNames, props);
+        }
+    }
+
+    public static UploadPartsResult newResultInstance(UploadPartsRequest request)
+            throws CommandException {
+        if (request instanceof PrepareUploadPartsRequest) {
+            return new PrepareUploadPartsResult();
+        } else if (request instanceof UploadPartsRequest) {
+            return new UploadPartsResult();
+        } else {
+            String cname = request.getName();
+            throw new CommandException("the type of this request is invalid: "
+                    + cname);
+        }
+    }
+
+    public static UploadPartsCommand newCommandInstance(
+            UploadPartsRequest request) throws CommandException {
+        if (request instanceof PrepareUploadPartsRequest) {
+            return new PrepareUploadPartsCommand();
+        } else if (request instanceof UploadPartsRequest) {
+            return new UploadPartsCommand<UploadPartsRequest, UploadPartsResult>();
+        } else {
+            String cname = request.getName();
+            throw new CommandException("the type of this request is invalid: "
+                    + cname);
+        }
+    }
+}
