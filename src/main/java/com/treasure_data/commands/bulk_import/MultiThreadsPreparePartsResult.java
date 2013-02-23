ec83afe0f918a274d31da54f6412283fadb59c09
Muga Nishizawa <muga.nishizawa@gmail.com>
2013-02-23 18:49:34 +0900

    added new optimized PreparePartsResult class with multi-threads

---
+++ src/main/java/com/treasure_data/commands/bulk_import/MultiThreadsPreparePartsResult.java
@@ -0,0 +1,57 @@
+//
+// Java Extension to CUI for Treasure Data
+//
+// Copyright (C) 2012 - 2013 Muga Nishizawa
+//
+//    Licensed under the Apache License, Version 2.0 (the "License");
+//    you may not use this file except in compliance with the License.
+//    You may obtain a copy of the License at
+//
+//        http://www.apache.org/licenses/LICENSE-2.0
+//
+//    Unless required by applicable law or agreed to in writing, software
+//    distributed under the License is distributed on an "AS IS" BASIS,
+//    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//    See the License for the specific language governing permissions and
+//    limitations under the License.
+//
+package com.treasure_data.commands.bulk_import;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import com.treasure_data.commands.CommandResult;
+
+public class MultiThreadsPreparePartsResult extends PreparePartsResult {
+
+    PrepareUploadPartsCommand command;
+
+    public MultiThreadsPreparePartsResult() {
+        super();
+    }
+
+    public synchronized void addOutputFilePath(String filePath) {
+        super.addOutputFilePath(filePath);
+        if (command != null) {
+            command.taskQueue.add(
+                    new PrepareUploadPartsCommand.Worker.Task(filePath));
+        }
+    }
+
+    public void setPrepareUploadPartsCommand(PrepareUploadPartsCommand command) {
+        this.command = command;
+    }
+
+    public Object clone() {
+        return new MultiThreadsPreparePartsResult();
+    }
+
+    public synchronized void addFinishTask(int numOfThreads) {
+        if (command != null) {
+            for (int i = 0; i < numOfThreads; i++) {
+                command.taskQueue
+                        .add(PrepareUploadPartsCommand.Worker.FINISH_TASK);
+            }
+        }
+    }
+}
