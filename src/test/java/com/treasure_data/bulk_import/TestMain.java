7df154e0d3d465259afc71f86257b840990babf6
Muga Nishizawa <muga.nishizawa@gmail.com>
2013-06-13 17:45:40 +0900

    minor change of TestMain class

--- src/test/java/com/treasure_data/bulk_import/TestMain.java
+++ src/test/java/com/treasure_data/bulk_import/TestMain.java
@@ -9,7 +9,7 @@ import com.treasure_data.bulk_import.Config;
 
 public class TestMain {
 
-    @Test //@Ignore
+    @Test @Ignore
     public void testPrepareParts01() throws Exception {
         Properties props = System.getProperties();
         props.load(this.getClass().getClassLoader()
@@ -46,23 +46,23 @@ public class TestMain {
         //props.setProperty(Config.BI_PREPARE_PARTS_TIMECOLUMN, "date_code");
         //props.setProperty(Config.BI_PREPARE_PARTS_ENCODING, "Shift_JIS");
         props.setProperty(Config.BI_PREPARE_PARTS_TIMEVALUE, "1370941200");
-        props.setProperty(Config.BI_PREPARE_PARTS_PARALLEL, "4");
-        props.setProperty(Config.BI_UPLOAD_PARTS_PARALLEL, "4");
+        props.setProperty(Config.BI_PREPARE_PARTS_PARALLEL, "1");
+        props.setProperty(Config.BI_UPLOAD_PARTS_PARALLEL, "1");
         final String[] args = new String[] {
                 "upload_parts",
                 "mugasess",
 //                "./in/TE_JNL_ITM_shiftJIS.csv",
                 "./in/from_SQLServer_to_csv_10_v01.csv",
-                "./in/from_SQLServer_to_csv_10_v02.csv",
-                "./in/from_SQLServer_to_csv_10_v03.csv",
-                "./in/from_SQLServer_to_csv_10_v04.csv",
-                "./in/from_SQLServer_to_csv_10_v05.csv",
-                "./in/from_SQLServer_to_csv_10_v06.csv",
-                "./in/from_SQLServer_to_csv_10_v07.csv",
-                "./in/from_SQLServer_to_csv_10_v08.csv",
-                "./in/from_SQLServer_to_csv_10_v09.csv",
+//                "./in/from_SQLServer_to_csv_10_v02.csv",
+//                "./in/from_SQLServer_to_csv_10_v03.csv",
+//                "./in/from_SQLServer_to_csv_10_v04.csv",
+//                "./in/from_SQLServer_to_csv_10_v05.csv",
+//                "./in/from_SQLServer_to_csv_10_v06.csv",
+//                "./in/from_SQLServer_to_csv_10_v07.csv",
+//                "./in/from_SQLServer_to_csv_10_v08.csv",
+//                "./in/from_SQLServer_to_csv_10_v09.csv",
         };
 
-        Main.uploadParts(args, props);
+        Main.prepareAndUploadParts(args, props);
     }
 }
