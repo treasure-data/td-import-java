5a555c867e2e25bed617498f7770ee1f4a60f2f1
Muga Nishizawa <muga.nishizawa@gmail.com>
2013-06-14 18:51:35 +0900

    added new file writer class 'MockFileWriter' for unit testing

---
+++ src/test/java/com/treasure_data/bulk_import/prepare_parts/writer/MockFileWriter.java
@@ -0,0 +1,89 @@
+package com.treasure_data.bulk_import.prepare_parts.writer;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import com.treasure_data.bulk_import.prepare_parts.PrepareConfig;
+import com.treasure_data.bulk_import.prepare_parts.PreparePartsException;
+
+public class MockFileWriter extends FileWriter {
+
+    private Map<String, Object> row;
+    private List<Object> columnKeyValues;
+
+    public MockFileWriter(PrepareConfig conf) {
+        super(conf);
+    }
+
+    @Override
+    protected void configure(String infileName) throws PreparePartsException {
+    }
+
+    @Override
+    public void writeBeginRow(int size) throws PreparePartsException {
+        if (row != null || columnKeyValues != null) {
+            throw new IllegalStateException("row must be null");
+        }
+        row = new HashMap<String, Object>();
+        columnKeyValues = new ArrayList<Object>();
+    }
+
+    @Override
+    public void write(Object v) throws PreparePartsException {
+        columnKeyValues.add(v);
+    }
+
+    @Override
+    public void writeString(String v) throws PreparePartsException {
+        columnKeyValues.add(v);
+    }
+
+    @Override
+    public void writeInt(int v) throws PreparePartsException {
+        columnKeyValues.add(v);
+    }
+
+    @Override
+    public void writeLong(long v) throws PreparePartsException {
+        columnKeyValues.add(v);
+    }
+
+    @Override
+    public void writeDouble(double v) throws PreparePartsException {
+        columnKeyValues.add(v);
+    }
+
+    @Override
+    public void writeNil() throws PreparePartsException {
+        columnKeyValues.add(null);
+    }
+
+    @Override
+    public void writeEndRow() throws PreparePartsException {
+        int size = columnKeyValues.size() / 2;
+        for (int i = 0; i < size; i++) {
+            String key = (String) columnKeyValues.get(2 * i);
+            Object val = columnKeyValues.get(2 * i + 1);
+            row.put(key, val);
+        }
+    }
+
+    @Override
+    public void close() throws IOException {
+        if (row != null) {
+            row.clear();
+            row = null;
+        }
+        if (columnKeyValues != null) {
+            columnKeyValues.clear();
+            columnKeyValues = null;
+        }
+    }
+
+    public Map<String, Object> getRow() {
+        return row;
+    }
+}
