07f025508bd228ee4216c752cd12ce0cc5953961
Muga Nishizawa <muga.nishizawa@gmail.com>
2013-06-22 15:05:06 +0900

    minor change of TestMain class

--- src/test/java/com/treasure_data/bulk_import/TestMain.java
+++ src/test/java/com/treasure_data/bulk_import/TestMain.java
@@ -9,19 +9,20 @@ import com.treasure_data.bulk_import.Configuration;
 
 public class TestMain {
 
-    @Test @Ignore
+    @Test @Ignore // TODO
     public void testPrepareParts01() throws Exception {
         Properties props = System.getProperties();
         props.load(this.getClass().getClassLoader()
                 .getResourceAsStream("treasure-data.properties"));
 
-        props.setProperty(Configuration.BI_PREPARE_PARTS_TIMECOLUMN, "date_code");
+        //props.setProperty(Configuration.BI_PREPARE_PARTS_TIMECOLUMN, "date_code");
         //props.setProperty(Config.BI_PREPARE_PARTS_ENCODING, "Shift_JIS");
-        //props.setProperty(Config.BI_PREPARE_PARTS_TIMEVALUE, "1370941200");
+        //props.setProperty(Configuration.BI_PREPARE_PARTS_TIMEVALUE, "1370941200");
         props.setProperty(Configuration.BI_PREPARE_PARTS_PARALLEL, "2");
         final String[] args = new String[] {
                 "prepare_parts",
                 "./in/sample.csv",
+                //"./in/sample2.csv", // for time-value
 //                "./in/TE_JNL_ITM_shiftJIS.csv",
 //                "./in/from_SQLServer_to_csv_10_v01.csv",
 //                "./in/from_SQLServer_to_csv_10_v02.csv",
