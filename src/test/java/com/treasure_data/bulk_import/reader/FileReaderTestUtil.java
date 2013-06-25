1c62c703829942b74ce559fa261386ea1f470f02
Muga Nishizawa <muga.nishizawa@gmail.com>
2013-06-25 15:11:21 +0900

    added test utilities for FileReader class

--- src/test/java/com/treasure_data/bulk_import/reader/FileReaderTestUtil.java
+++ src/test/java/com/treasure_data/bulk_import/reader/FileReaderTestUtil.java
@@ -1,6 +1,8 @@
 package com.treasure_data.bulk_import.reader;
 
+import java.util.Date;
 import java.util.Properties;
+import java.util.Random;
 
 import org.junit.After;
 import org.junit.Before;
@@ -8,44 +10,84 @@ import org.junit.Ignore;
 import org.junit.Test;
 
 import com.treasure_data.bulk_import.prepare_parts.PrepareConfiguration;
+import com.treasure_data.bulk_import.prepare_parts.PrepareProcessor;
 import com.treasure_data.bulk_import.prepare_parts.writer.FileWriterTestUtil;
 import com.treasure_data.bulk_import.writer.FileWriter;
 
 @Ignore
 public class FileReaderTestUtil {
 
+    protected long baseTime;
+
     protected Properties props;
     protected PrepareConfiguration conf;
+
+    protected FileReader reader;
     protected FileWriter writer;
 
-    protected CSVFileReader reader;
+    protected PrepareProcessor.Task task;
+
+    protected Random rand = new Random(new Random().nextInt());
 
     @Before
     public void createResources() throws Exception {
-        props = System.getProperties();
+        baseTime = new Date().getTime() / 1000 / 3600 * 3600;
+
+        // create properties
+        createProperties();
 
         // create configuration
-        conf = new PrepareConfiguration();
-        conf.configure(props);
+        createPrepareConfiguration();
 
         // create writer
+        createFileWriter();
+
+        // create reader
+        createFileReader();
+
+        // create prepare task
+        createTask();
+    }
+
+    protected void createProperties() throws Exception {
+        props = System.getProperties();
+    }
+
+    protected void createPrepareConfiguration() throws Exception {
+        conf = new PrepareConfiguration();
+        conf.configure(props);
+    }
+
+    protected void createFileWriter() throws Exception {
         writer = new FileWriterTestUtil(conf);
+    }
+
+    protected void createFileReader() throws Exception {
+        // implement it in subclasses
+    }
+
+    protected void createTask() throws Exception {
+        // implement it in subclasses
     }
 
     @After
     public void destroyResources() throws Exception {
+        destroyFileWriter();
+
+        destroyFileReader();
+
+    }
+
+    protected void destroyFileWriter() throws Exception {
         if (writer != null) {
             writer.close();
         }
+    }
 
+    protected void destroyFileReader() throws Exception {
         if (reader != null) {
             reader.close();
         }
-    }
-
-    @Test
-    public void NextCalled() throws Exception {
-        
     }
 
 }
