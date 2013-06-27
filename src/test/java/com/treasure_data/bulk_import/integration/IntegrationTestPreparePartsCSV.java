d8a73850fb7f241f987402598d5cce3737cc11d4
Muga Nishizawa <muga.nishizawa@gmail.com>
2013-06-27 18:00:04 +0900

    changed integration test for csv prepare_parts -- added new test method

--- src/test/java/com/treasure_data/bulk_import/integration/IntegrationTestPreparePartsCSV.java
+++ src/test/java/com/treasure_data/bulk_import/integration/IntegrationTestPreparePartsCSV.java
@@ -19,7 +19,7 @@ public class IntegrationTestPreparePartsCSV extends PreparePartsIntegrationTestU
         super.destroyResources();
     }
 
-    @Test
+    @Test @Ignore
     public void writeFromCSVWithTimeColumn() throws Exception {
         props.setProperty(Configuration.BI_PREPARE_PARTS_FORMAT, "csv");
         props.setProperty(Configuration.BI_PREPARE_PARTS_COMPRESSION, "auto");
@@ -36,4 +36,21 @@ public class IntegrationTestPreparePartsCSV extends PreparePartsIntegrationTestU
         assertDataEquals(srcFileName, dstFileName);
     }
 
+    @Test @Ignore
+    public void writeFromCSVWithAlasTimeColumn() throws Exception {
+        props.setProperty(Configuration.BI_PREPARE_PARTS_FORMAT, "csv");
+        props.setProperty(Configuration.BI_PREPARE_PARTS_COMPRESSION, "auto");
+        props.setProperty(Configuration.BI_PREPARE_PARTS_OUTPUTDIR, OUTPUT_DIR);
+        props.setProperty(Configuration.BI_PREPARE_PARTS_COLUMNHEADER, "true");
+        props.setProperty(Configuration.BI_PREPARE_PARTS_TIMECOLUMN, "timestamp");
+
+        args.add(Configuration.CMD_PREPARE_PARTS);
+        args.add(INPUT_DIR + "csvfile-with-aliastime.csv");
+
+        Main.prepareParts(args.toArray(new String[0]), props);
+
+        String srcFileName = INPUT_DIR + "trainingfile-with-time.msgpack.gz";
+        String dstFileName = OUTPUT_DIR + "csvfile-with-aliastime_csv_0.msgpack.gz";
+        assertDataEquals(srcFileName, dstFileName);
+    }
 }
