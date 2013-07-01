dcd7753ad5bc6c86b60ab37fcf58f5067ca43a9d
Muga Nishizawa <muga.nishizawa@gmail.com>
2013-07-01 19:38:00 +0900

    minor change of FileReader class -- deleted unused import statements in the class

--- src/main/java/com/treasure_data/bulk_import/reader/FileReader.java
+++ src/main/java/com/treasure_data/bulk_import/reader/FileReader.java
@@ -21,9 +21,7 @@ import java.io.Closeable;
 import java.io.IOException;
 import java.io.OutputStream;
 import java.io.PrintWriter;
-import java.util.ArrayList;
 import java.util.HashSet;
-import java.util.List;
 import java.util.Set;
 import java.util.logging.Logger;
 
