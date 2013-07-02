eca212a6ecbae61bfbc9c3a845e1e95c707005e0
Muga Nishizawa <muga.nishizawa@gmail.com>
2013-07-02 20:57:46 +0900

    minor change

--- src/test/java/com/treasure_data/bulk_import/integration/IntegrationTestPreparePartsCSV.java
+++ src/test/java/com/treasure_data/bulk_import/integration/IntegrationTestPreparePartsCSV.java
@@ -19,116 +19,116 @@ public class IntegrationTestPreparePartsCSV extends PreparePartsIntegrationTestU
         super.destroyResources();
     }
 
-//    @Test
-//    public void writeFromCSVWithTimeColumn() throws Exception {
-//        props.setProperty(Configuration.BI_PREPARE_PARTS_FORMAT, "csv");
-//        props.setProperty(Configuration.BI_PREPARE_PARTS_COMPRESSION, "auto");
-//        props.setProperty(Configuration.BI_PREPARE_PARTS_OUTPUTDIR, OUTPUT_DIR);
-//        props.setProperty(Configuration.BI_PREPARE_PARTS_COLUMNHEADER, "true");
-//
-//        args.add(Configuration.CMD_PREPARE_PARTS);
-//        args.add(INPUT_DIR + "csvfile-with-time.csv");
-//
-//        Main.prepareParts(args.toArray(new String[0]), props);
-//
-//        String srcFileName = INPUT_DIR + "trainingfile-with-time.msgpack.gz";
-//        String dstFileName = OUTPUT_DIR + "csvfile-with-time_csv_0.msgpack.gz";
-//        assertDataEquals(srcFileName, dstFileName);
-//    }
-//
-//    @Test
-//    public void writeFromCSVWithAlasTimeColumn() throws Exception {
-//        props.setProperty(Configuration.BI_PREPARE_PARTS_FORMAT, "csv");
-//        props.setProperty(Configuration.BI_PREPARE_PARTS_COMPRESSION, "auto");
-//        props.setProperty(Configuration.BI_PREPARE_PARTS_OUTPUTDIR, OUTPUT_DIR);
-//        props.setProperty(Configuration.BI_PREPARE_PARTS_COLUMNHEADER, "true");
-//        props.setProperty(Configuration.BI_PREPARE_PARTS_TIMECOLUMN, "timestamp");
-//
-//        args.add(Configuration.CMD_PREPARE_PARTS);
-//        args.add(INPUT_DIR + "csvfile-with-aliastime.csv");
-//
-//        Main.prepareParts(args.toArray(new String[0]), props);
-//
-//        String srcFileName = INPUT_DIR + "trainingfile-with-time.msgpack.gz";
-//        String dstFileName = OUTPUT_DIR + "csvfile-with-aliastime_csv_0.msgpack.gz";
-//        assertDataEquals(srcFileName, dstFileName);
-//    }
-//
-//    @Test
-//    public void writeFromCSVWithTimeFormat() throws Exception {
-//        props.setProperty(Configuration.BI_PREPARE_PARTS_FORMAT, "csv");
-//        props.setProperty(Configuration.BI_PREPARE_PARTS_COMPRESSION, "auto");
-//        props.setProperty(Configuration.BI_PREPARE_PARTS_OUTPUTDIR, OUTPUT_DIR);
-//        props.setProperty(Configuration.BI_PREPARE_PARTS_COLUMNHEADER, "true");
-//        props.setProperty(Configuration.BI_PREPARE_PARTS_TIMECOLUMN, "timeformat");
-//        props.setProperty(Configuration.BI_PREPARE_PARTS_TIMEFORMAT, "%Y-%m-%d %H:%M:%S %z");
-//
-//        args.add(Configuration.CMD_PREPARE_PARTS);
-//        args.add(INPUT_DIR + "csvfile-with-timeformat.csv");
-//
-//        Main.prepareParts(args.toArray(new String[0]), props);
-//
-//        String srcFileName = INPUT_DIR + "trainingfile-with-time.msgpack.gz";
-//        String dstFileName = OUTPUT_DIR + "csvfile-with-timeformat_csv_0.msgpack.gz";
-//        assertDataEquals(srcFileName, dstFileName);
-//    }
-//
-//    @Test
-//    public void writeFromHeaderlessCSVWithTimeColumn() throws Exception {
-//        props.setProperty(Configuration.BI_PREPARE_PARTS_FORMAT, "csv");
-//        props.setProperty(Configuration.BI_PREPARE_PARTS_COMPRESSION, "auto");
-//        props.setProperty(Configuration.BI_PREPARE_PARTS_OUTPUTDIR, OUTPUT_DIR);
-//        props.setProperty(Configuration.BI_PREPARE_PARTS_COLUMNHEADER, "false");
-//        props.setProperty(Configuration.BI_PREPARE_PARTS_COLUMNS, "string-value,int-value,double-value,timestamp,time");
-//
-//        args.add(Configuration.CMD_PREPARE_PARTS);
-//        args.add(INPUT_DIR + "headerless-csvfile-with-time.csv");
-//
-//        Main.prepareParts(args.toArray(new String[0]), props);
-//
-//        String srcFileName = INPUT_DIR + "trainingfile-with-time.msgpack.gz";
-//        String dstFileName = OUTPUT_DIR + "headerless-csvfile-with-time_csv_0.msgpack.gz";
-//        assertDataEquals(srcFileName, dstFileName);
-//    }
-//
-//    @Test
-//    public void writeFromHeaderlessCSVWithAlasTimeColumn() throws Exception {
-//        props.setProperty(Configuration.BI_PREPARE_PARTS_FORMAT, "csv");
-//        props.setProperty(Configuration.BI_PREPARE_PARTS_COMPRESSION, "auto");
-//        props.setProperty(Configuration.BI_PREPARE_PARTS_OUTPUTDIR, OUTPUT_DIR);
-//        props.setProperty(Configuration.BI_PREPARE_PARTS_COLUMNHEADER, "false");
-//        props.setProperty(Configuration.BI_PREPARE_PARTS_COLUMNS, "string-value,int-value,double-value,timestamp");
-//        props.setProperty(Configuration.BI_PREPARE_PARTS_TIMECOLUMN, "timestamp");
-//
-//        args.add(Configuration.CMD_PREPARE_PARTS);
-//        args.add(INPUT_DIR + "headerless-csvfile-with-aliastime.csv");
-//
-//        Main.prepareParts(args.toArray(new String[0]), props);
-//
-//        String srcFileName = INPUT_DIR + "trainingfile-with-time.msgpack.gz";
-//        String dstFileName = OUTPUT_DIR + "headerless-csvfile-with-aliastime_csv_0.msgpack.gz";
-//        assertDataEquals(srcFileName, dstFileName);
-//    }
-//
-//    @Test
-//    public void writeFromHeaderlessCSVWithTimeFormat() throws Exception {
-//        props.setProperty(Configuration.BI_PREPARE_PARTS_FORMAT, "csv");
-//        props.setProperty(Configuration.BI_PREPARE_PARTS_COMPRESSION, "auto");
-//        props.setProperty(Configuration.BI_PREPARE_PARTS_OUTPUTDIR, OUTPUT_DIR);
-//        props.setProperty(Configuration.BI_PREPARE_PARTS_COLUMNHEADER, "false");
-//        props.setProperty(Configuration.BI_PREPARE_PARTS_COLUMNS, "string-value,int-value,double-value,timeformat");
-//        props.setProperty(Configuration.BI_PREPARE_PARTS_TIMECOLUMN, "timeformat");
-//        props.setProperty(Configuration.BI_PREPARE_PARTS_TIMEFORMAT, "%Y-%m-%d %H:%M:%S %z");
-//
-//        args.add(Configuration.CMD_PREPARE_PARTS);
-//        args.add(INPUT_DIR + "headerless-csvfile-with-timeformat.csv");
-//
-//        Main.prepareParts(args.toArray(new String[0]), props);
-//
-//        String srcFileName = INPUT_DIR + "trainingfile-with-time.msgpack.gz";
-//        String dstFileName = OUTPUT_DIR + "headerless-csvfile-with-timeformat_csv_0.msgpack.gz";
-//        assertDataEquals(srcFileName, dstFileName);
-//    }
+    @Test
+    public void writeFromCSVWithTimeColumn() throws Exception {
+        props.setProperty(Configuration.BI_PREPARE_PARTS_FORMAT, "csv");
+        props.setProperty(Configuration.BI_PREPARE_PARTS_COMPRESSION, "auto");
+        props.setProperty(Configuration.BI_PREPARE_PARTS_OUTPUTDIR, OUTPUT_DIR);
+        props.setProperty(Configuration.BI_PREPARE_PARTS_COLUMNHEADER, "true");
+
+        args.add(Configuration.CMD_PREPARE_PARTS);
+        args.add(INPUT_DIR + "csvfile-with-time.csv");
+
+        Main.prepareParts(args.toArray(new String[0]), props);
+
+        String srcFileName = INPUT_DIR + "trainingfile-with-time.msgpack.gz";
+        String dstFileName = OUTPUT_DIR + "csvfile-with-time_csv_0.msgpack.gz";
+        assertDataEquals(srcFileName, dstFileName);
+    }
+
+    @Test
+    public void writeFromCSVWithAlasTimeColumn() throws Exception {
+        props.setProperty(Configuration.BI_PREPARE_PARTS_FORMAT, "csv");
+        props.setProperty(Configuration.BI_PREPARE_PARTS_COMPRESSION, "auto");
+        props.setProperty(Configuration.BI_PREPARE_PARTS_OUTPUTDIR, OUTPUT_DIR);
+        props.setProperty(Configuration.BI_PREPARE_PARTS_COLUMNHEADER, "true");
+        props.setProperty(Configuration.BI_PREPARE_PARTS_TIMECOLUMN, "timestamp");
+
+        args.add(Configuration.CMD_PREPARE_PARTS);
+        args.add(INPUT_DIR + "csvfile-with-aliastime.csv");
+
+        Main.prepareParts(args.toArray(new String[0]), props);
+
+        String srcFileName = INPUT_DIR + "trainingfile-with-time.msgpack.gz";
+        String dstFileName = OUTPUT_DIR + "csvfile-with-aliastime_csv_0.msgpack.gz";
+        assertDataEquals(srcFileName, dstFileName);
+    }
+
+    @Test
+    public void writeFromCSVWithTimeFormat() throws Exception {
+        props.setProperty(Configuration.BI_PREPARE_PARTS_FORMAT, "csv");
+        props.setProperty(Configuration.BI_PREPARE_PARTS_COMPRESSION, "auto");
+        props.setProperty(Configuration.BI_PREPARE_PARTS_OUTPUTDIR, OUTPUT_DIR);
+        props.setProperty(Configuration.BI_PREPARE_PARTS_COLUMNHEADER, "true");
+        props.setProperty(Configuration.BI_PREPARE_PARTS_TIMECOLUMN, "timeformat");
+        props.setProperty(Configuration.BI_PREPARE_PARTS_TIMEFORMAT, "%Y-%m-%d %H:%M:%S %z");
+
+        args.add(Configuration.CMD_PREPARE_PARTS);
+        args.add(INPUT_DIR + "csvfile-with-timeformat.csv");
+
+        Main.prepareParts(args.toArray(new String[0]), props);
+
+        String srcFileName = INPUT_DIR + "trainingfile-with-time.msgpack.gz";
+        String dstFileName = OUTPUT_DIR + "csvfile-with-timeformat_csv_0.msgpack.gz";
+        assertDataEquals(srcFileName, dstFileName);
+    }
+
+    @Test
+    public void writeFromHeaderlessCSVWithTimeColumn() throws Exception {
+        props.setProperty(Configuration.BI_PREPARE_PARTS_FORMAT, "csv");
+        props.setProperty(Configuration.BI_PREPARE_PARTS_COMPRESSION, "auto");
+        props.setProperty(Configuration.BI_PREPARE_PARTS_OUTPUTDIR, OUTPUT_DIR);
+        props.setProperty(Configuration.BI_PREPARE_PARTS_COLUMNHEADER, "false");
+        props.setProperty(Configuration.BI_PREPARE_PARTS_COLUMNS, "string-value,int-value,double-value,timestamp,time");
+
+        args.add(Configuration.CMD_PREPARE_PARTS);
+        args.add(INPUT_DIR + "headerless-csvfile-with-time.csv");
+
+        Main.prepareParts(args.toArray(new String[0]), props);
+
+        String srcFileName = INPUT_DIR + "trainingfile-with-time.msgpack.gz";
+        String dstFileName = OUTPUT_DIR + "headerless-csvfile-with-time_csv_0.msgpack.gz";
+        assertDataEquals(srcFileName, dstFileName);
+    }
+
+    @Test
+    public void writeFromHeaderlessCSVWithAlasTimeColumn() throws Exception {
+        props.setProperty(Configuration.BI_PREPARE_PARTS_FORMAT, "csv");
+        props.setProperty(Configuration.BI_PREPARE_PARTS_COMPRESSION, "auto");
+        props.setProperty(Configuration.BI_PREPARE_PARTS_OUTPUTDIR, OUTPUT_DIR);
+        props.setProperty(Configuration.BI_PREPARE_PARTS_COLUMNHEADER, "false");
+        props.setProperty(Configuration.BI_PREPARE_PARTS_COLUMNS, "string-value,int-value,double-value,timestamp");
+        props.setProperty(Configuration.BI_PREPARE_PARTS_TIMECOLUMN, "timestamp");
+
+        args.add(Configuration.CMD_PREPARE_PARTS);
+        args.add(INPUT_DIR + "headerless-csvfile-with-aliastime.csv");
+
+        Main.prepareParts(args.toArray(new String[0]), props);
+
+        String srcFileName = INPUT_DIR + "trainingfile-with-time.msgpack.gz";
+        String dstFileName = OUTPUT_DIR + "headerless-csvfile-with-aliastime_csv_0.msgpack.gz";
+        assertDataEquals(srcFileName, dstFileName);
+    }
+
+    @Test
+    public void writeFromHeaderlessCSVWithTimeFormat() throws Exception {
+        props.setProperty(Configuration.BI_PREPARE_PARTS_FORMAT, "csv");
+        props.setProperty(Configuration.BI_PREPARE_PARTS_COMPRESSION, "auto");
+        props.setProperty(Configuration.BI_PREPARE_PARTS_OUTPUTDIR, OUTPUT_DIR);
+        props.setProperty(Configuration.BI_PREPARE_PARTS_COLUMNHEADER, "false");
+        props.setProperty(Configuration.BI_PREPARE_PARTS_COLUMNS, "string-value,int-value,double-value,timeformat");
+        props.setProperty(Configuration.BI_PREPARE_PARTS_TIMECOLUMN, "timeformat");
+        props.setProperty(Configuration.BI_PREPARE_PARTS_TIMEFORMAT, "%Y-%m-%d %H:%M:%S %z");
+
+        args.add(Configuration.CMD_PREPARE_PARTS);
+        args.add(INPUT_DIR + "headerless-csvfile-with-timeformat.csv");
+
+        Main.prepareParts(args.toArray(new String[0]), props);
+
+        String srcFileName = INPUT_DIR + "trainingfile-with-time.msgpack.gz";
+        String dstFileName = OUTPUT_DIR + "headerless-csvfile-with-timeformat_csv_0.msgpack.gz";
+        assertDataEquals(srcFileName, dstFileName);
+    }
 
     @Test
     public void writeFromJSONWithTimeColumn() throws Exception {
