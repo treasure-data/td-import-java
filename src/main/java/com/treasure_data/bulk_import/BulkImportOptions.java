82980d3662df43aaeaf592324d78d55e5f26e837
Muga Nishizawa <muga.nishizawa@gmail.com>
2013-08-06 19:33:57 +0900

    minor change of BulkImportOptions class -- deleted unused import statements

--- src/main/java/com/treasure_data/bulk_import/BulkImportOptions.java
+++ src/main/java/com/treasure_data/bulk_import/BulkImportOptions.java
@@ -20,7 +20,6 @@ package com.treasure_data.bulk_import;
 import java.io.IOException;
 import java.util.Arrays;
 import java.util.HashSet;
-import java.util.List;
 import java.util.Map;
 import java.util.Properties;
 import java.util.Set;
