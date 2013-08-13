9fa2f1ca8eb7fa0bb412a1d41aa7f3fa0aa08ec9
Muga Nishizawa <muga.nishizawa@gmail.com>
2013-08-13 13:56:25 +0900

    changed TestBulkImportMain class

--- src/test/java/com/treasure_data/bulk_import/TestBulkImportMain.java
+++ src/test/java/com/treasure_data/bulk_import/TestBulkImportMain.java
@@ -1,5 +1,7 @@
 package com.treasure_data.bulk_import;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Properties;
 
 import org.junit.Ignore;
@@ -15,81 +17,78 @@ public class TestBulkImportMain {
         props.load(this.getClass().getClassLoader()
                 .getResourceAsStream("treasure-data.properties"));
 
-        props.setProperty(Configuration.BI_PREPARE_PARTS_TIMECOLUMN, "date_code");
+        List<String> opts = new ArrayList<String>();
         //props.setProperty(Config.BI_PREPARE_PARTS_ENCODING, "Shift_JIS");
         //props.setProperty(Configuration.BI_PREPARE_PARTS_TIMEVALUE, "1370941200");
-        props.setProperty(Configuration.BI_PREPARE_PARTS_PARALLEL, "2");
-        final String[] args = new String[] {
-                "prepare_parts",
-                "in/from_SQLServer_to_csv_10000000_v01.csv",
-                //"./in/sample.csv",
-                //"./in/sample2.csv", // for time-value
-//                "./in/TE_JNL_ITM_shiftJIS.csv",
-//                "./in/from_SQLServer_to_csv_10_v01.csv",
-//                "./in/from_SQLServer_to_csv_10_v02.csv",
-//                "./in/from_SQLServer_to_csv_10_v03.csv",
-//                "./in/from_SQLServer_to_csv_10_v04.csv",
-//                "./in/from_SQLServer_to_csv_10_v05.csv",
-//                "./in/from_SQLServer_to_csv_10_v06.csv",
-//                "./in/from_SQLServer_to_csv_10_v07.csv",
-//                "./in/from_SQLServer_to_csv_10_v08.csv",
-//                "./in/from_SQLServer_to_csv_10_v09.csv",
-        };
+        opts.add("--time-column");
+        opts.add("date_code");
+        opts.add("--prepare-parallel");
+        opts.add("2");
+        opts.add("--column-header");
+        List<String> args = new ArrayList<String>();
+        args.add("prepare_parts");
+        args.add("in/from_SQLServer_to_csv_10000000_v01.csv");
+//        args.add("./in/from_SQLServer_to_csv_10_v01.csv");
+//        args.add("./in/from_SQLServer_to_csv_10_v02.csv");
+//        args.add("./in/from_SQLServer_to_csv_10_v03.csv");
+//        args.add("./in/from_SQLServer_to_csv_10_v04.csv");
+//        args.add("./in/from_SQLServer_to_csv_10_v05.csv");
+//        args.add("./in/from_SQLServer_to_csv_10_v06.csv");
+//        args.add("./in/from_SQLServer_to_csv_10_v07.csv");
+//        args.add("./in/from_SQLServer_to_csv_10_v08.csv");
+//        args.add("./in/from_SQLServer_to_csv_10_v09.csv");
+//        args.add("./in/TE_JNL_ITM_shiftJIS.csv");
 
-        BulkImportMain.prepareParts(args, props);
+        args.addAll(opts);
+
+        BulkImportMain.prepareParts(args.toArray(new String[0]), props);
     }
 
-    @Test @Ignore
-    public void testPrepareParts02() throws Exception {
-        Properties props = System.getProperties();
-        props.load(this.getClass().getClassLoader()
-                .getResourceAsStream("treasure-data.properties"));
+//    @Test @Ignore
+//    public void testPrepareParts02() throws Exception {
+//        Properties props = System.getProperties();
+//        props.load(this.getClass().getClassLoader()
+//                .getResourceAsStream("treasure-data.properties"));
+//
+//        //props.setProperty(Configuration.BI_PREPARE_PARTS_TIMECOLUMN, "date_code");
+//        //props.setProperty(Config.BI_PREPARE_PARTS_ENCODING, "Shift_JIS");
+//        //props.setProperty(Configuration.BI_PREPARE_PARTS_TIMEVALUE, "1370941200");
+//        //props.setProperty(Configuration.BI_PREPARE_PARTS_PARALLEL, "2");
+//        props.setProperty(Configuration.BI_PREPARE_PARTS_FORMAT, "mysql");
+//        props.setProperty(Configuration.BI_PREPARE_PARTS_JDBC_CONNECTION_URL, props.getProperty("mysql.test.url"));
+//        props.setProperty(Configuration.BI_PREPARE_PARTS_JDBC_USER, props.getProperty("mysql.test.user"));
+//        props.setProperty(Configuration.BI_PREPARE_PARTS_JDBC_PASSWORD, props.getProperty("mysql.test.password"));
+//        props.setProperty(Configuration.BI_PREPARE_PARTS_JDBC_TABLE, props.getProperty("mysql.test.table"));
+//        final String[] args = new String[] {
+//                "prepare_parts",
+//                "mugatbl"
+//        };
+//
+//        BulkImportMain.prepareParts(args, props);
+//    }
 
-        //props.setProperty(Configuration.BI_PREPARE_PARTS_TIMECOLUMN, "date_code");
-        //props.setProperty(Config.BI_PREPARE_PARTS_ENCODING, "Shift_JIS");
-        //props.setProperty(Configuration.BI_PREPARE_PARTS_TIMEVALUE, "1370941200");
-        //props.setProperty(Configuration.BI_PREPARE_PARTS_PARALLEL, "2");
-        props.setProperty(Configuration.BI_PREPARE_PARTS_FORMAT, "mysql");
-        props.setProperty(Configuration.BI_PREPARE_PARTS_JDBC_CONNECTION_URL, props.getProperty("mysql.test.url"));
-        props.setProperty(Configuration.BI_PREPARE_PARTS_JDBC_USER, props.getProperty("mysql.test.user"));
-        props.setProperty(Configuration.BI_PREPARE_PARTS_JDBC_PASSWORD, props.getProperty("mysql.test.password"));
-        props.setProperty(Configuration.BI_PREPARE_PARTS_JDBC_TABLE, props.getProperty("mysql.test.table"));
-        final String[] args = new String[] {
-                "prepare_parts",
-                "mugatbl"
-        };
-
-        BulkImportMain.prepareParts(args, props);
-    }
-
-    @Test @Ignore
+    @Test
     public void testUploadParts01() throws Exception {
         Properties props = System.getProperties();
         props.load(this.getClass().getClassLoader()
                 .getResourceAsStream("treasure-data.properties"));
 
+        List<String> opts = new ArrayList<String>();
         //props.setProperty(Config.BI_PREPARE_PARTS_TIMECOLUMN, "date_code");
         //props.setProperty(Config.BI_PREPARE_PARTS_ENCODING, "Shift_JIS");
-        props.setProperty(Configuration.BI_PREPARE_PARTS_TIMEVALUE, "1370941200");
-        props.setProperty(Configuration.BI_UPLOAD_PARTS_PARALLEL, "1");
-        props.setProperty(Configuration.BI_UPLOAD_PARTS_AUTO_PERFORM, "false");
-        final String[] args = new String[] {
-                "upload_parts",
-                "mugasess",
-//                "./in/TE_JNL_ITM_shiftJIS.csv",
-                //"./in/from_SQLServer_to_csv_10_v01.csv",
-                "./out/from_SQLServer_to_csv_10000000_v01_csv_0.msgpack.gz",
-//                "./in/from_SQLServer_to_csv_10_v02.csv",
-//                "./in/from_SQLServer_to_csv_10_v03.csv",
-//                "./in/from_SQLServer_to_csv_10_v04.csv",
-//                "./in/from_SQLServer_to_csv_10_v05.csv",
-//                "./in/from_SQLServer_to_csv_10_v06.csv",
-//                "./in/from_SQLServer_to_csv_10_v07.csv",
-//                "./in/from_SQLServer_to_csv_10_v08.csv",
-//                "./in/from_SQLServer_to_csv_10_v09.csv",
-        };
+        opts.add("--parallel");
+        opts.add("1");
 
-        BulkImportMain.uploadParts(args, props);
+        List<String> args = new ArrayList<String>();
+        args.add("upload_parts");
+        args.add("mugasess");
+        //args.add("out/from_SQLServer_to_csv_10000000_v01_csv_0.msgpack.gz");
+        args.add("out/from_SQLServer_to_csv_10_v01_csv_0.msgpack.gz");
+        args.add("out/from_SQLServer_to_csv_10_v02_csv_0.msgpack.gz");
+
+        args.addAll(opts);
+
+        BulkImportMain.uploadParts(args.toArray(new String[0]), props);
     }
 
     @Test @Ignore
@@ -98,53 +97,47 @@ public class TestBulkImportMain {
         props.load(this.getClass().getClassLoader()
                 .getResourceAsStream("treasure-data.properties"));
 
-        props.setProperty(Configuration.BI_PREPARE_PARTS_TIMECOLUMN, "date_code");
+        List<String> opts = new ArrayList<String>();
+        opts.add("--column-header");
+        opts.add("--time-column");
+        opts.add("date_code");
         //props.setProperty(Config.BI_PREPARE_PARTS_ENCODING, "Shift_JIS");
         //props.setProperty(Configuration.BI_PREPARE_PARTS_TIMEVALUE, "1370941200");
-        props.setProperty(Configuration.BI_PREPARE_PARTS_PARALLEL, "8");
-        props.setProperty(Configuration.BI_UPLOAD_PARTS_AUTO_PERFORM, "false");
-        final String[] args = new String[] {
-                "upload_parts",
-                "mugasess",
-                //"in/from_SQLServer_to_csv_10000000_v01.csv",
-                //"./in/sample.csv",
-                //"./in/sample2.csv", // for time-value
-//                "./in/TE_JNL_ITM_shiftJIS.csv",
-                "./in/from_SQLServer_to_csv_10_v01.csv",
-                "./in/from_SQLServer_to_csv_10_v02.csv",
-//                "./in/from_SQLServer_to_csv_10_v03.csv",
-//                "./in/from_SQLServer_to_csv_10_v04.csv",
-//                "./in/from_SQLServer_to_csv_10_v05.csv",
-//                "./in/from_SQLServer_to_csv_10_v06.csv",
-//                "./in/from_SQLServer_to_csv_10_v07.csv",
-//                "./in/from_SQLServer_to_csv_10_v08.csv",
-//                "./in/from_SQLServer_to_csv_10_v09.csv",
-        };
+        opts.add("--prepare-parallel");
+        opts.add("8");
 
-        BulkImportMain.prepareAndUploadParts(args, props);
+        List<String> args = new ArrayList<String>();
+        args.add("upload_parts");
+        args.add("mugasess");
+        args.add("./in/from_SQLServer_to_csv_10_v01.csv");
+        args.add("./in/from_SQLServer_to_csv_10_v02.csv");
+
+        args.addAll(opts);
+
+        BulkImportMain.uploadParts(args.toArray(new String[0]), props);
     }
 
-    @Test @Ignore
-    public void testPrepareUploadParts02() throws Exception {
-        Properties props = System.getProperties();
-        props.load(this.getClass().getClassLoader()
-                .getResourceAsStream("treasure-data.properties"));
-
-        //props.setProperty(Configuration.BI_PREPARE_PARTS_TIMECOLUMN, "date_code");
-        //props.setProperty(Config.BI_PREPARE_PARTS_ENCODING, "Shift_JIS");
-        //props.setProperty(Configuration.BI_PREPARE_PARTS_TIMEVALUE, "1370941200");
-        //props.setProperty(Configuration.BI_PREPARE_PARTS_PARALLEL, "2");
-        props.setProperty(Configuration.BI_PREPARE_PARTS_FORMAT, "mysql");
-        props.setProperty(Configuration.BI_PREPARE_PARTS_JDBC_CONNECTION_URL, props.getProperty("mysql.test.url"));
-        props.setProperty(Configuration.BI_PREPARE_PARTS_JDBC_USER, props.getProperty("mysql.test.user"));
-        props.setProperty(Configuration.BI_PREPARE_PARTS_JDBC_PASSWORD, props.getProperty("mysql.test.password"));
-        props.setProperty(Configuration.BI_PREPARE_PARTS_JDBC_TABLE, props.getProperty("mysql.test.table"));
-        final String[] args = new String[] {
-                "upload_parts",
-                "mugasess",
-                "mugatbl"
-        };
-
-        BulkImportMain.prepareAndUploadParts(args, props);
-    }
+//    @Test @Ignore
+//    public void testPrepareUploadParts02() throws Exception {
+//        Properties props = System.getProperties();
+//        props.load(this.getClass().getClassLoader()
+//                .getResourceAsStream("treasure-data.properties"));
+//
+//        //props.setProperty(Configuration.BI_PREPARE_PARTS_TIMECOLUMN, "date_code");
+//        //props.setProperty(Config.BI_PREPARE_PARTS_ENCODING, "Shift_JIS");
+//        //props.setProperty(Configuration.BI_PREPARE_PARTS_TIMEVALUE, "1370941200");
+//        //props.setProperty(Configuration.BI_PREPARE_PARTS_PARALLEL, "2");
+//        props.setProperty(Configuration.BI_PREPARE_PARTS_FORMAT, "mysql");
+//        props.setProperty(Configuration.BI_PREPARE_PARTS_JDBC_CONNECTION_URL, props.getProperty("mysql.test.url"));
+//        props.setProperty(Configuration.BI_PREPARE_PARTS_JDBC_USER, props.getProperty("mysql.test.user"));
+//        props.setProperty(Configuration.BI_PREPARE_PARTS_JDBC_PASSWORD, props.getProperty("mysql.test.password"));
+//        props.setProperty(Configuration.BI_PREPARE_PARTS_JDBC_TABLE, props.getProperty("mysql.test.table"));
+//        final String[] args = new String[] {
+//                "upload_parts",
+//                "mugasess",
+//                "mugatbl"
+//        };
+//
+//        BulkImportMain.prepareAndUploadParts(args, props);
+//    }
 }
