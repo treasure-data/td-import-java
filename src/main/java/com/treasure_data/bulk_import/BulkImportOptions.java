685972439787577bec69bd7cc1ee330645bf2c5d
Muga Nishizawa <muga.nishizawa@gmail.com>
2013-08-14 18:01:41 +0900

    changed BulkImportOptions and Constants classes -- refactoring

--- src/main/java/com/treasure_data/bulk_import/BulkImportOptions.java
+++ src/main/java/com/treasure_data/bulk_import/BulkImportOptions.java
@@ -145,96 +145,113 @@ public class BulkImportOptions {
         op.formatHelpWith(new SimpleHelpFormatter());
         op.acceptsAll(Arrays.asList("h", "help"),
                 "show this help message");
-
-        op.acceptsAll(Arrays.asList("f", "format"),
-                "source file format [csv, tsv, json, msgpack]; default=csv")
+        op.acceptsAll(Arrays.asList("f",
+                Configuration.BI_PREPARE_PARTS_FORMAT),
+                Configuration.BI_PREPARE_PARTS_FORMAT_DESC)
                 .withRequiredArg()
                 .describedAs("FORMAT")
                 .ofType(String.class);
-        op.acceptsAll(Arrays.asList("C", "compress"),
-                "compressed type [gzip, none]; default=auto detect")
+        op.acceptsAll(Arrays.asList("C",
+                Configuration.BI_PREPARE_PARTS_COMPRESSION),
+                Configuration.BI_PREPARE_PARTS_COMPRESSION_DESC)
                 .withRequiredArg()
                 .describedAs("TYPE")
                 .ofType(String.class);
-        op.acceptsAll(Arrays.asList("e", "encoding"),
-                "encoding type [utf-8]")
+        op.acceptsAll(Arrays.asList("e",
+                Configuration.BI_PREPARE_PARTS_ENCODING),
+                Configuration.BI_PREPARE_PARTS_ENCODING_DESC)
                 .withRequiredArg()
                 .describedAs("TYPE")
                 .ofType(String.class);
-        op.acceptsAll(Arrays.asList("t", "time-column"),
-                "name of the time column")
+        op.acceptsAll(Arrays.asList("t",
+                Configuration.BI_PREPARE_PARTS_TIMECOLUMN),
+                Configuration.BI_PREPARE_PARTS_TIMECOLUMN_DESC)
                 .withRequiredArg()
                 .describedAs("NAME")
                 .ofType(String.class);
-        op.acceptsAll(Arrays.asList("T", "time-format"),
-                "STRF_FORMAT; default=auto detect")
+        op.acceptsAll(Arrays.asList("T",
+                Configuration.BI_PREPARE_PARTS_TIMEFORMAT),
+                Configuration.BI_PREPARE_PARTS_TIMEFORMAT_DESC)
                 .withRequiredArg()
                 .withValuesSeparatedBy(",")
                 .describedAs("FORMAT")
                 .ofType(String.class);
-        op.acceptsAll(Arrays.asList("time-value"),
-                "long value of the time column")
+        op.acceptsAll(Arrays.asList(
+                Configuration.BI_PREPARE_PARTS_TIMEVALUE),
+                Configuration.BI_PREPARE_PARTS_TIMEVALUE_DESC)
                 .withRequiredArg()
                 .describedAs("TIME")
                 .ofType(String.class);
-        op.acceptsAll(Arrays.asList("o", "output"),
-                "output directory")
+        op.acceptsAll(Arrays.asList("o",
+                Configuration.BI_PREPARE_PARTS_OUTPUTDIR),
+                Configuration.BI_PREPARE_PARTS_OUTPUTDIR_DESC)
                 .withRequiredArg()
                 .describedAs("DIR")
                 .ofType(String.class);
-        op.acceptsAll(Arrays.asList("s", "split-size"),
-                "size of each parts (default: 16384)")
+        op.acceptsAll(Arrays.asList("s",
+                Configuration.BI_PREPARE_PARTS_SPLIT_SIZE),
+                Configuration.BI_PREPARE_PARTS_SPLIT_SIZE_DESC )
                 .withRequiredArg()
                 .describedAs("SIZE_IN_KB")
                 .ofType(String.class);
-        op.acceptsAll(Arrays.asList("error-records-handling"),
-                "error records handling mode [skip, abort]; default=skip")
+        op.acceptsAll(Arrays.asList(
+                Configuration.BI_PREPARE_PARTS_ERROR_RECORDS_HANDLING),
+                Configuration.BI_PREPARE_PARTS_ERROR_RECORDS_HANDLING_DESC)
                 .withRequiredArg()
                 .describedAs("MODE")
                 .ofType(String.class);
-        op.acceptsAll(Arrays.asList("delimiter"),
-                "delimiter CHAR; default=\",\" at csv, \"\\t\" at tsv")
+        op.acceptsAll(Arrays.asList(
+                Configuration.BI_PREPARE_PARTS_DELIMITER),
+                Configuration.BI_PREPARE_PARTS_DELIMITER_DESC)
                 .withRequiredArg()
                 .describedAs("CHAR")
                 .ofType(String.class);
-        op.acceptsAll(Arrays.asList("quote"),
-                "quote [DOUBLE, SINGLE]; default=DOUBLE")
+        op.acceptsAll(Arrays.asList(
+                Configuration.BI_PREPARE_PARTS_QUOTE),
+                Configuration.BI_PREPARE_PARTS_QUOTE_DESC)
                 .withRequiredArg()
                 .describedAs("CHAR")
                 .ofType(String.class);
-        op.acceptsAll(Arrays.asList("newline"),
-                "newline [CRLR, LR, CR];  default=CRLF")
+        op.acceptsAll(Arrays.asList(
+                Configuration.BI_PREPARE_PARTS_NEWLINE),
+                Configuration.BI_PREPARE_PARTS_NEWLINE_DESC)
                 .withRequiredArg()
                 .describedAs("TYPE")
                 .ofType(String.class);
-        op.acceptsAll(Arrays.asList("column-header"),
-                "first line includes column names");
-        op.acceptsAll(Arrays.asList("columns"),
-                "column names (use --column-header instead if the first line has column names)")
+        op.acceptsAll(Arrays.asList(
+                Configuration.BI_PREPARE_PARTS_COLUMNHEADER),
+                Configuration.BI_PREPARE_PARTS_COLUMNHEADER_DESC);
+        op.acceptsAll(Arrays.asList(
+                Configuration.BI_PREPARE_PARTS_COLUMNS),
+                Configuration.BI_PREPARE_PARTS_COLUMNS_DESC)
                 .withRequiredArg()
                 .describedAs("NAME,NAME,...")
                 .ofType(String.class)
                 .withValuesSeparatedBy(",");
-        op.acceptsAll(Arrays.asList("column-types"),
-                "column types [string, int, long]")
+        op.acceptsAll(Arrays.asList(
+                Configuration.BI_PREPARE_PARTS_COLUMNTYPES),
+                Configuration.BI_PREPARE_PARTS_COLUMNTYPES_DESC)
                 .withRequiredArg()
                 .describedAs("TYPE,TYPE,...")
                 .ofType(String.class)
                 .withValuesSeparatedBy(",");
-        op.acceptsAll(Arrays.asList("exclude-columns"),
-                "exclude columns")
+        op.acceptsAll(Arrays.asList(
+                Configuration.BI_PREPARE_PARTS_EXCLUDE_COLUMNS),
+                Configuration.BI_PREPARE_PARTS_EXCLUDE_COLUMNS_DESC)
                 .withRequiredArg()
                 .describedAs("NAME,NAME,...")
                 .ofType(String.class)
                 .withValuesSeparatedBy(",");
-        op.acceptsAll(Arrays.asList("only-columns"),
-                "only-columns")
+        op.acceptsAll(Arrays.asList(
+                Configuration.BI_PREPARE_PARTS_ONLY_COLUMNS),
+                Configuration.BI_PREPARE_PARTS_ONLY_COLUMNS_DESC)
                 .withRequiredArg()
                 .describedAs("NAME,NAME,...")
                 .ofType(String.class)
                 .withValuesSeparatedBy(",");
-        op.acceptsAll(Arrays.asList("prepare-parallel"),
-                "prepare in parallel (default: 2; max 8)")
+        op.acceptsAll(Arrays.asList(
+                Configuration.BI_PREPARE_PARTS_PARALLEL),
+                Configuration.BI_PREPARE_PARTS_PARALLEL_DESC)
                 .withRequiredArg()
                 .describedAs("NUM")
                 .ofType(String.class);
@@ -243,12 +260,15 @@ public class BulkImportOptions {
 
     public void initUploadOptionParser(Properties props) {
         this.initPrepareOptionParser(props);
-        op.acceptsAll(Arrays.asList("auto-perform"),
-                "perform bulk import job automatically");
-        op.acceptsAll(Arrays.asList("auto-commit"),
-                "commit bulk import job automatically");
-        op.acceptsAll(Arrays.asList("parallel"),
-                "upload in parallel (default: 2; max 8)")
+        op.acceptsAll(Arrays.asList(
+                Configuration.BI_UPLOAD_PARTS_AUTO_PERFORM),
+                Configuration.BI_UPLOAD_PARTS_AUTO_PERFORM_DESC);
+        op.acceptsAll(Arrays.asList(
+                Configuration.BI_UPLOAD_PARTS_AUTO_COMMIT),
+                Configuration.BI_UPLOAD_PARTS_AUTO_COMMIT_DESC);
+        op.acceptsAll(Arrays.asList(
+                Configuration.BI_UPLOAD_PARTS_PARALLEL),
+                Configuration.BI_UPLOAD_PARTS_PARALLEL_DESC)
                 .withRequiredArg()
                 .describedAs("NUM")
                 .ofType(String.class);
--- src/main/java/com/treasure_data/bulk_import/Constants.java
+++ src/main/java/com/treasure_data/bulk_import/Constants.java
@@ -32,14 +32,20 @@ public interface Constants extends com.treasure_data.client.Constants {
     String BI_UPLOAD_PARTS_FORMAT_DEFAULTVALUE = "msgpack.gz"; // default 'msgpack.gz'
 
     // auto-perform
+    String BI_UPLOAD_PARTS_AUTO_PERFORM = "auto-perform";
     String BI_UPLOAD_PARTS_AUTO_PERFORM_DEFAULTVALUE = "false";
+    String BI_UPLOAD_PARTS_AUTO_PERFORM_DESC = "perform bulk import job automatically";
 
     // auto-commit
+    String BI_UPLOAD_PARTS_AUTO_COMMIT = "auto-commit";
     String BI_UPLOAD_PARTS_AUTO_COMMIT_DEFAULTVALUE = "false";
+    String BI_UPLOAD_PARTS_AUTO_COMMIT_DESC = "commit bulk import job automatically";
 
     // parallel NUM
+    String BI_UPLOAD_PARTS_PARALLEL = "parallel";
     String BI_UPLOAD_PARTS_PARALLEL_DEFAULTVALUE = "2";
     String BI_UPLOAD_PARTS_PARALLEL_MAX_VALUE = "8";
+    String BI_UPLOAD_PARTS_PARALLEL_DESC = "upload in parallel (default: 2; max 8)";
 
     // retryCount NUM
     String BI_UPLOAD_PARTS_RETRYCOUNT = "td.bulk_import.upload_parts.retrycount";
@@ -54,55 +60,97 @@ public interface Constants extends com.treasure_data.client.Constants {
     ////////////////////////////////////////
 
     // format [csv, tsv, json, msgpack, apache, regexp]; default=auto detect
+    String BI_PREPARE_PARTS_FORMAT = "format";
     String BI_PREPARE_PARTS_FORMAT_DEFAULTVALUE = "csv"; // default 'csv'
+    String BI_PREPARE_PARTS_FORMAT_DESC = "source file format [csv, tsv, json, msgpack]; default=csv";
 
     // output format [msgpackgz]; default=msgpackgz
     String BI_PREPARE_PARTS_OUTPUTFORMAT = "td.bulk_import.prepare_parts.outputformat";
     String BI_PREPARE_PARTS_OUTPUTFORMAT_DEFAULTVALUE = "msgpackgz";
 
     // compress [gzip,.., auto]; default=auto detect
+    String BI_PREPARE_PARTS_COMPRESSION = "compress";
     String BI_PREPARE_PARTS_COMPRESSION_DEFAULTVALUE = "auto";
+    String BI_PREPARE_PARTS_COMPRESSION_DESC = "compressed type [gzip, none]; default=auto detect";
 
     // parallel
+    String BI_PREPARE_PARTS_PARALLEL = "prepare-parallel";
     String BI_PREPARE_PARTS_PARALLEL_DEFAULTVALUE = "1";
+    String BI_PREPARE_PARTS_PARALLEL_DESC = "prepare in parallel (default: 2; max 8)";
 
     // encoding [utf-8,...]
+    String BI_PREPARE_PARTS_ENCODING = "encoding";
     String BI_PREPARE_PARTS_ENCODING_DEFAULTVALUE = "UTF-8";
+    String BI_PREPARE_PARTS_ENCODING_DESC = "encoding type [utf-8]";
+
+    // columns, column-types
+    String BI_PREPARE_PARTS_COLUMNS = "columns";
+    String BI_PREPARE_PARTS_COLUMNS_DESC = "column names (use --column-header instead if the first line has column names)";
+    String BI_PREPARE_PARTS_COLUMNTYPES = "column-types";
+    String BI_PREPARE_PARTS_COLUMNTYPES_DESC = "column types [string, int, long]";
+
+    // exclude-columns, only-columns
+    String BI_PREPARE_PARTS_EXCLUDE_COLUMNS = "exclude-columns";
+    String BI_PREPARE_PARTS_EXCLUDE_COLUMNS_DESC = "exclude columns";
+    String BI_PREPARE_PARTS_ONLY_COLUMNS = "only-columns";
+    String BI_PREPARE_PARTS_ONLY_COLUMNS_DESC = "only columns";
 
     // time-column NAME; default='time'
+    String BI_PREPARE_PARTS_TIMECOLUMN = "time-column";
     String BI_PREPARE_PARTS_TIMECOLUMN_DEFAULTVALUE = "time";
+    String BI_PREPARE_PARTS_TIMECOLUMN_DESC = "name of the time column";
+
+    // time-value
+    String BI_PREPARE_PARTS_TIMEVALUE = "time-value";
+    String BI_PREPARE_PARTS_TIMEVALUE_DESC = "long value of the time column";
 
     // time-format STRF_FORMAT; default=auto detect
+    String BI_PREPARE_PARTS_TIMEFORMAT = "time-format";
     String BI_PREPARE_PARTS_TIMEFORMAT_DEFAULTVALUE = "auto";
+    String BI_PREPARE_PARTS_TIMEFORMAT_DESC = "STRF_FORMAT; default=auto detect";
 
     // output DIR
+    String BI_PREPARE_PARTS_OUTPUTDIR = "output";
     String BI_PREPARE_PARTS_OUTPUTDIR_DEFAULTVALUE = "out"; // './out/'
+    String BI_PREPARE_PARTS_OUTPUTDIR_DESC = "output directory";
 
     // error handling
+    String BI_PREPARE_PARTS_ERROR_RECORDS_HANDLING = "error-records-handling";
     String BI_PREPARE_PARTS_ERROR_RECORDS_HANDLING_DEFAULTVALUE= "skip";
+    String BI_PREPARE_PARTS_ERROR_RECORDS_HANDLING_DESC = "error records handling mode [skip, abort]; default=skip";
 
     // dry-run; show samples as JSON and exit
     String BI_PREPARE_PARTS_DRYRUN = "td.bulk_import.prepare_parts.dry-run";
     String BI_PREPARE_PARTS_DRYRUN_DEFAULTVALUE = "false";
 
+    String BI_PREPARE_PARTS_SPLIT_SIZE = "split-size";
     String BI_PREPARE_PARTS_SPLIT_SIZE_DEFAULTVALUE ="16384";
+    String BI_PREPARE_PARTS_SPLIT_SIZE_DESC = "size of each parts (default: 16384)";
 
     ////////////////////////////////////////
     // CSV/TSV PREPARE_PARTS_OPTIONS      //
     ////////////////////////////////////////
 
     // quote [DOUBLE, SINGLE]; default=DOUBLE
+    String BI_PREPARE_PARTS_QUOTE = "quote";
     String BI_PREPARE_PARTS_QUOTE_DEFAULTVALUE = "DOUBLE";
+    String BI_PREPARE_PARTS_QUOTE_DESC = "quote [DOUBLE, SINGLE]; default=DOUBLE";
 
     // delimiter CHAR; default=',' at 'csv', '\t' at 'tsv'
+    String BI_PREPARE_PARTS_DELIMITER = "delimiter";
     String BI_PREPARE_PARTS_DELIMITER_CSV_DEFAULTVALUE = ",";
     String BI_PREPARE_PARTS_DELIMITER_TSV_DEFAULTVALUE = "\t";
+    String BI_PREPARE_PARTS_DELIMITER_DESC = "delimiter CHAR; default=\",\" at csv, \"\\t\" at tsv";
 
     // newline [CRLF, LF, CR]; default=CRLF (or auto detect?)
+    String BI_PREPARE_PARTS_NEWLINE = "newline";
     String BI_PREPARE_PARTS_NEWLINE_DEFAULTVALUE = "CRLF"; // default CRLF
+    String BI_PREPARE_PARTS_NEWLINE_DESC = "newline [CRLR, LR, CR];  default=CRLF";
 
     // column-header; default=true
+    String BI_PREPARE_PARTS_COLUMNHEADER = "column-header";
     String BI_PREPARE_PARTS_COLUMNHEADER_DEFAULTVALUE = "false";
+    String BI_PREPARE_PARTS_COLUMNHEADER_DESC = "first line includes column names";
 
     // type-conversion-error [skip,null]; default=skip
     String BI_PREPARE_PARTS_TYPE_CONVERSION_ERROR_DEFAULTVALUE = "skip";
