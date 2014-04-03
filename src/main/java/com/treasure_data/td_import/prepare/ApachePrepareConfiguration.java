bbf31ff9e902673fb31dd549b65bab0e2c490a2f
Muga Nishizawa <muga.nishizawa@gmail.com>
2014-04-03 12:22:48 -0700

    minor change: deleted debug codes

--- src/main/java/com/treasure_data/td_import/prepare/ApachePrepareConfiguration.java
+++ src/main/java/com/treasure_data/td_import/prepare/ApachePrepareConfiguration.java
@@ -47,7 +47,6 @@ public class ApachePrepareConfiguration extends RegexPrepareConfiguration {
         public void convertType(String v, ColumnValue into)
                 throws PreparePartsException {
             if (v.equals("-")) {
-                System.out.println("### convert");
                 into.parse("0");
             } else {
                 into.parse(v);
