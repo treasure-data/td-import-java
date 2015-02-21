1c82d660f5a0a2a68ddf86f5c314b92344fe2d9c
Muga Nishizawa <muga.nishizawa@gmail.com>
2015-02-21 00:14:29 -0800

    renamed 'empty-as-null' option to 'empty-as-null-if-numeric'

--- src/main/java/com/treasure_data/td_import/Constants.java
+++ src/main/java/com/treasure_data/td_import/Constants.java
@@ -95,7 +95,7 @@ public interface Constants extends com.treasure_data.client.Constants {
             "    --column-types TYPE,TYPE,...     column types [string, int, long, double]\n" +
             "    --column-type NAME:TYPE          column type [string, int, long, double]. A pair of column name and type can be specified like 'age:int'\n" +
             "    -S, --all-string                 disable automatic type conversion\n" +
-            "    --empty-as-null                  the empty string values are interpreted as null values if columns are numerical types." +
+            "    --empty-as-null-if-numeric       the empty string values are interpreted as null values if columns are numerical types." +
             "\n" +
             "    CSV/TSV specific options:\n" +
             "    --column-header                  first line includes column names\n" +
@@ -296,8 +296,8 @@ public interface Constants extends com.treasure_data.client.Constants {
     String BI_PREPARE_ALL_STRING_HYPHEN = HYPHENHYPHEN + BI_PREPARE_PARTS_COLUMNS;
     String BI_PREPARE_ALL_STRING_DESC = "disable automatic type conversion";
 
-    // empty-as-null; default=false
-    String BI_PREPARE_PARTS_EMPTYASNULL = "empty-as-null";
+    // empty-as-null-if-numeric; default=false
+    String BI_PREPARE_PARTS_EMPTYASNULL = "empty-as-null-if-numeric";
     String BI_PREPARE_EMPTYASNULL_HYPHEN = HYPHENHYPHEN + BI_PREPARE_PARTS_EMPTYASNULL;
     String BI_PREPARE_PARTS_EMPTYASNULL_DEFAULTVALUE = "false";
     String BI_PREPARE_PARTS_EMPTYASNULL_DESC = "the empty string values are interpreted as null values if columns arenumerical types";
